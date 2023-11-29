54888ea04ebd340eed3ee7a466a7948b3019b6de
samolin <samolin.vlad@gmail.com>
2023-11-29 22:34:40 +0200

    Add a new field to the book's serializer showing amount of books belong to the author

--- library/books/serializers.py
+++ library/books/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Book
 
+
 class BookSerializer(serializers.ModelSerializer):
+    author_books_count = serializers.SerializerMethodField()
+
+    def get_author_books_count(self, instance):
+        return Book.objects.filter(author=instance.author).count()
+
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'publication_year', 'isbn']
+        fields = ['id', 'title', 'author', 'publication_year', 'isbn', 'author_books_count']
