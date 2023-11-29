87f4be3882f21c92d4899b2feb225190f595d699
samolin <samolin.vlad@gmail.com>
2023-11-29 16:56:12 +0200

    Add books endpoints, using generic classes

--- library/books/views.py
+++ library/books/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
